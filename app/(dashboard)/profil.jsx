75cf592d5a2b563c0e9e90564219a399af5d7409
voldemor44 <ayiloamendavid@gmail.com>
2024-12-26 23:18:39 +0100

    commit

--- app/(dashboard)/profil.jsx
+++ app/(dashboard)/profil.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const profil = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>profil</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
