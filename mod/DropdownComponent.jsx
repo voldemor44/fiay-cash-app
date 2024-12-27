80c800a75d1efa311f750af9191e030a334c0184
voldemor44 <ayiloamendavid@gmail.com>
2024-12-27 17:06:32 +0100

    commit

--- mod/DropdownComponent.jsx
+++ mod/DropdownComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
 import AntDesign from "@expo/vector-icons/AntDesign";
 
