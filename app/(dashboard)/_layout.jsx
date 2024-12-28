0e20776d7bbe04e91a8eca1c964d8b756d84d8c9
voldemor44 <ayiloamendavid@gmail.com>
2024-12-28 16:26:44 +0100

    commit

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,36 +1,40 @@
 import { View, Text } from "react-native";
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import React from "react";
 import { Drawer } from "expo-router/drawer";
 
 const DashboardLayout = () => {
   return (
-    <Drawer
-      screenOptions={{
-        headerShown: false,
-        drawerStyle: {
-          backgroundColor: "#f9f9f9", // Couleur d'arrière-plan du Drawer
-          width: 250, // Largeur du Drawer
-        },
-        drawerLabelStyle: {
-          fontSize: 16, // Taille du texte des labels
-          fontWeight: "bold", // Poids de la police
-        },
-        drawerActiveTintColor: "green", // Couleur du texte actif
-        drawerInactiveTintColor: "gray", // Couleur du texte inactif
-        drawerActiveBackgroundColor: "transparent", // Couleur de fond actif
-      }}
-    >
-      <Drawer.Screen name="index" options={{ drawerLabel: "Dashboard" }} />
-      <Drawer.Screen name="profil" options={{ drawerLabel: "Profil" }} />
-      <Drawer.Screen
-        name="historique"
-        options={{ drawerLabel: "Historique" }}
-      />
-      <Drawer.Screen
-        name="send-request"
-        options={{ drawerLabel: "Vérification ID" }}
-      />
-    </Drawer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          headerShown: false,
+          drawerStyle: {
+            backgroundColor: "#f9f9f9",
+            width: 250,
+          },
+          drawerLabelStyle: {
+            fontSize: 16,
+            fontWeight: "bold",
+          },
+          drawerActiveTintColor: "green",
+          drawerInactiveTintColor: "gray", // Couleur du texte inactif
+          drawerActiveBackgroundColor: "transparent",
+        }}
+      >
+        <Drawer.Screen name="index" options={{ drawerLabel: "Dashboard" }} />
+        <Drawer.Screen name="profil" options={{ drawerLabel: "Profil" }} />
+        <Drawer.Screen
+          name="historique"
+          options={{ drawerLabel: "Historique" }}
+        />
+        <Drawer.Screen
+          name="send-request"
+          options={{ drawerLabel: "Vérification ID" }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 };
 
