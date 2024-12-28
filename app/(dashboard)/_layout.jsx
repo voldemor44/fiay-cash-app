331c5afa3b8790d4a1ee9225599d6dbc9d82e013
voldemor44 <ayiloamendavid@gmail.com>
2024-12-28 19:37:42 +0100

    commit

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -3,6 +3,7 @@ import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import React from "react";
 import { Drawer } from "expo-router/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 const DashboardLayout = () => {
   return (
@@ -23,15 +24,41 @@ const DashboardLayout = () => {
           drawerActiveBackgroundColor: "transparent",
         }}
       >
-        <Drawer.Screen name="index" options={{ drawerLabel: "Dashboard" }} />
-        <Drawer.Screen name="profil" options={{ drawerLabel: "Profil" }} />
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: "Dashboard",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="home-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="profil"
+          options={{
+            drawerLabel: "Profil",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="person-outline" size={size} color={color} />
+            ),
+          }}
+        />
         <Drawer.Screen
           name="historique"
-          options={{ drawerLabel: "Historique" }}
+          options={{
+            drawerLabel: "Historique",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="library-outline" size={size} color={color} />
+            ),
+          }}
         />
         <Drawer.Screen
           name="send-request"
-          options={{ drawerLabel: "Vérification ID" }}
+          options={{
+            drawerLabel: "Vérification ID",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="checkbox-outline" size={size} color={color} />
+            ),
+          }}
         />
       </Drawer>
     </GestureHandlerRootView>
