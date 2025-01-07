b8c0749ae359110c04a7de4ea038c52d751d602c
voldemor44 <ayiloamendavid@gmail.com>
2025-01-07 09:55:15 +0100

    commit

--- app/(admin)/payment-page.jsx
+++ app/(admin)/payment-page.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const AdminLayout = () => {
+const PaymentPage = () => {
   return (
     <View>
-      <Text>AdminLayout</Text>
+      <Text>PaymentPage</Text>
     </View>
   );
 };
 
-export default AdminLayout;
+export default PaymentPage;
--- app/(admin)/requests-table.jsx
+++ app/(admin)/requests-table.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const AdminLayout = () => {
+const RequestsTable = () => {
   return (
     <View>
-      <Text>AdminLayout</Text>
+      <Text>RequestsTable</Text>
     </View>
   );
 };
 
-export default AdminLayout;
+export default RequestsTable;
